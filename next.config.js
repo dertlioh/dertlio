d3a3ec463a2e670fdb9e90624ded75a8d39da182
dertlioh <obasogluu@gmail.com>
2025-07-20 00:01:58 +0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
-const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,  },
-  typescript: {    ignoreBuildErrors: false,  }};
+const nextConfig = {  output: "export",
+  images: {    unoptimized: true,  },  typescript: {    ignoreBuildErrors: false,
+  },};
 module.exports = nextConfig;
-```**`tailwind.config.js` dosyası oluştur:**
-```javascriptmodule.exports = {
-  content: [    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-  ],  theme: {
-    extend: {
-      fontFamily: {        pacifico: ['var(--font-pacifico)', 'serif'],      },    },
-  },
-  plugins: [],
-}```
